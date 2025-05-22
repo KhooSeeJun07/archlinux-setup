7bb0161bc70cc5134c5908f906dd662ea4cc0c89
khooseejun <160625964+KhooSeeJun07@users.noreply.github.com>
2025-05-22 20:04:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,15 +19,15 @@ sudo pacman -S --needed --noconfirm noto-fonts noto-fonts-cjk papirus-icon-theme
 yay -S --needed --noconfirm bibata-cursor-theme-bin
 
 mkdir ~/.config/hypr
-mv ./config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-mv ./config/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
+cp ./config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+cp ./config/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
 mkdir ~/.config/kitty
-mv ./config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp ./config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 mkdir ~/.config/rofi
-mv ./config/rofi/config.rasi ~/.config/rofi/config.rasi
+cp ./config/rofi/config.rasi ~/.config/rofi/config.rasi
 mkdir ~/.config/waybar
-mv ./config/waybar/config ~/.config/waybar/config
-mv ./config/waybar/style.css ~/.config/waybar/style.css
+cp ./config/waybar/config ~/.config/waybar/config
+cp ./config/waybar/style.css ~/.config/waybar/style.css
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
