2bd781bb51a22b3c674d03dabc71b3b122a2c0db
khooseejun <160625964+KhooSeeJun07@users.noreply.github.com>
2025-05-22 19:55:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,10 +18,14 @@ cava \
 sudo pacman -S --needed --noconfirm noto-fonts noto-fonts-cjk papirus-icon-theme
 yay -S --needed --noconfirm bibata-cursor-theme-bin
 
+mkdir ~/.config/hypr
 mv ./config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 mv ./config/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
+mkdir ~/.config/kitty
 mv ./config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+mkdir ~/.config/rofi
 mv ./config/rofi/config.rasi ~/.config/rofi/config.rasi
+mkdir ~/.config/waybar
 mv ./config/waybar/config ~/.config/waybar/config
 mv ./config/waybar/style.css ~/.config/waybar/style.css
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
